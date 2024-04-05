2fed812a89fbe6aa5a765d001c145a8a8f797518
rh-fran6 <francis.anyaegbu@francisanyaegbu.local>
2024-04-04 21:13:50 -0500

    ddd

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var inMemoryStore = make(map[string]string)
 var redirectURL = "http://0.0.0.0:9000"
-fmt.Println("Hello")
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
